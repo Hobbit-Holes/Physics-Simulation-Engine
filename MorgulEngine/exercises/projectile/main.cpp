93a6c5e3849559850ededb6bd22eafbf6d7a7e45
Borja Sanz de Bremond O'Neill <borjasbon@gmail.com>
2023-03-15 13:31:28 +0100

    Change the way it works

--- MorgulEngine/exercises/projectile/main.cpp
+++ MorgulEngine/exercises/projectile/main.cpp
@@ -9,16 +9,27 @@ int main(int argc, char *argv[]) {
 
     // Object
     const auto ball = engine.world.create();
-    engine.world.emplace<TransformComponent>(ball, Vec2(width/2, heigth/2));
-    engine.world.emplace<KinematicComponent>(ball, Vec2(0, -150), Vec2(0, 75));
+    engine.world.emplace<TransformComponent>(ball, Vec2(50, heigth/2));
+    engine.world.emplace<KinematicComponent>(ball, Vec2(75, -150), Vec2(0, 75));
 
     while (engine.NextFrame()) {
         Graphics::CleanUpScreen();
         engine.Update();
 
         // Logic
-        const auto transform = engine.world.get<TransformComponent>(ball);
-        Graphics::DrawFillCircle(transform.position.x, transform.position.y, 25, Color::Cyan());
+        auto& transform = engine.world.get<TransformComponent>(ball);
+
+        if (transform.position.y > heigth/2) {
+            transform.position.y = heigth / 2;
+        }
+        if (transform.position.x >= width) {
+            transform.position.x = width;
+        }
+
+        // Rendering
+        Graphics::DrawFillRect(width/2, heigth/4, width, heigth/2, Color(119, 207, 242));              // Sky
+        Graphics::DrawFillRect(width/2, 3*heigth/4, width, heigth/2, Color(119, 166, 58));      // Ground
+        Graphics::DrawFillCircle(transform.position.x, transform.position.y, 15, Color::Red());
 
         engine.Render();
     }
