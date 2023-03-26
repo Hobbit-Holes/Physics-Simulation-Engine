2fa13ebbc624b392321d13ff5f73fc2c70881ff6
lauferhab <lferrerhaba@al.uloyola.es>
2023-03-26 23:51:19 +0200

    update_README

--- MorgulEngine/exercises/runge-kutta/main.cpp
+++ MorgulEngine/exercises/runge-kutta/main.cpp
@@ -5,11 +5,11 @@
 // Hay que buscar la funcion adecuada
 void rhs(double t, double *y, double *dydt) {
     dydt[0] = y[1];
-    dydt[1] = -y[0];
+    dydt[1] = y[0];
 }
 
 int main() {
-    double y0[2] = {500.0, 0.0};
+    double y0[2] = {1.27, 0.0};
 
     RungeKutta rk(2, &rhs);
     rk.x[0] = y0[0];
@@ -18,10 +18,10 @@ int main() {
     double t0 = 0.0;
     double dt = 0.01;
 
-    while(t0 <= 10) {
+    while(t0 < 10) {
         rk.Step(t0, dt);
-        t0 += dt;
         std::cout << "t = " << t0 << "\tx:" << rk.x[0] << "\tx(real) = " << rk._x[0] << std::endl;
+        t0 += dt;
     }
 
     return 0;
