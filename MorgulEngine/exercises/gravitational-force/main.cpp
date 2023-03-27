b38938d33abf9f5a63c326bdd47c01174498d89f
Borja Sanz de Bremond O'Neill <borjasbon@gmail.com>
2023-03-27 09:03:54 +0200

    Upgrade gravitational example

--- MorgulEngine/exercises/gravitational-force/main.cpp
+++ MorgulEngine/exercises/gravitational-force/main.cpp
@@ -1,35 +1,58 @@
 #include "MorgulEngine.hh"
 
 int main(int argc, char *argv[]) {
-    int width = 600;
-    int heigth = 400;
+    int width = 800;
+    int heigth = 800;
+
+    int numPlanets = 4;
+    if (argc == 2) {
+        numPlanets = strtol(argv[1], nullptr, 0);
+    }
 
     // Initialize Game Engine
     MorgulEngine engine = MorgulEngine(width, heigth);
 
     // Objects
-    const auto earth = engine.world.create();
-    engine.world.emplace<TransformComponent>(earth, Vec2(100, 100));
-    engine.world.emplace<KinematicComponent>(earth, Vec2(150, 0));
-    engine.world.emplace<ParticleComponent>(earth, 1, 10, Color::Green());
-
     const auto sun = engine.world.create();
     engine.world.emplace<TransformComponent>(sun, Vec2(width/2, heigth/2));
     engine.world.emplace<KinematicComponent>(sun);
-    engine.world.emplace<ParticleComponent>(sun, 20, 30, Color::Orange());
+    engine.world.emplace<ParticleComponent>(sun, 100, 30, Color::Orange());
 
-    auto& tEarth = engine.world.get<TransformComponent>(earth);
-    auto& pEarth = engine.world.get<ParticleComponent>(earth);
-    auto& tSun = engine.world.get<TransformComponent>(sun);
-    auto& pSun = engine.world.get<ParticleComponent>(sun);
+    std::vector<entt::entity> planets;
+    for (int i = 0; i < numPlanets; i++) {
+        Vec2 position = position.FromModuleAngle(150, (2* M_PI / numPlanets) * (i));
+        Vec2 velocity = position.UnitVector().Normal() * 200;
+        Color color = Color::Lerp(Color:: Green(), Color:: Cyan(), (float)i/(float)numPlanets);
+
+        const auto planet = engine.world.create();
+        engine.world.emplace<TransformComponent>(planet, position + Vec2(width/2, heigth/2));
+        engine.world.emplace<KinematicComponent>(planet, velocity);
+        engine.world.emplace<ParticleComponent>(planet, 10, 10, color);
+
+        planets.push_back(planet);
+    }
+    planets.push_back(sun);
 
     while (engine.NextFrame()) {
         Graphics::CleanUpScreen();
         engine.Update();
 
-        Vec2 gravitationalForce = Force::GenerateGravitationalVector(tEarth.position, tSun.position, pEarth.mass, pSun.mass, 1000, 5, 100);
-        pEarth.AddForce(gravitationalForce);
-        pSun.AddForce(-gravitationalForce);
+        for (auto& planet: planets) {
+            auto i = &planet - &planets[0];
+            auto& tPlanet = engine.world.get<TransformComponent>(planet);
+            auto& pPlanet = engine.world.get<ParticleComponent>(planet);
+
+            for (auto& planetAffected: planets) {
+                auto j = &planetAffected - &planets[0];
+                auto& tPlanetAffected = engine.world.get<TransformComponent>(planetAffected);
+                auto& pPlanetAffected = engine.world.get<ParticleComponent>(planetAffected);
+
+                if (i != j) {
+                    Vec2 gravitationalForce = Force::GenerateGravitationalVector(tPlanet.position, tPlanetAffected.position, pPlanet.mass, pPlanetAffected.mass, 50, 10, 2000);
+                    pPlanet.AddForce(gravitationalForce);
+                }
+            }
+        }
 
         engine.Render();
     }
