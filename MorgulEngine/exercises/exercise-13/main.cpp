6765ca447a0fc08a0c0d22e7eafe9c8539d3c8de
Borja Sanz de Bremond O'Neill <borjasbon@gmail.com>
2023-04-07 20:40:18 +0200

    Improve exercise 13

--- MorgulEngine/exercises/exercise-13/main.cpp
+++ MorgulEngine/exercises/exercise-13/main.cpp
@@ -6,10 +6,10 @@ int main(int argc, char *argv[]) {
 
     MorgulEngine engine = MorgulEngine(width, heigth);
 
-    float numObjectsUp = 1;
-    float numObjectsDown = 1;
-    float numObjectsRight = 1;
-    float numObjectsLeft = 1;
+    float numObjectsUp = 2;
+    float numObjectsDown = 2;
+    float numObjectsRight = 2;
+    float numObjectsLeft = 2;
     if (argc == 5) {
         numObjectsUp = strtol(argv[1], nullptr, 0);
         numObjectsDown = strtol(argv[2], nullptr, 0);
@@ -18,6 +18,8 @@ int main(int argc, char *argv[]) {
     }
 
     // Objects
+    float speedObject = 50;
+
     std::vector<entt::entity> objectsUp;
     for (int i = 1; i <= numObjectsUp; i++) {
         RegularPolygonShape obj = RegularPolygonShape(15, i+4, Color::Cyan(), false);
@@ -25,7 +27,7 @@ int main(int argc, char *argv[]) {
 
         const auto object = engine.world.create();
         engine.world.emplace<TransformComponent>(object, Vec2(100, 75) + Vec2(600, 0) * (i - 1) / (numObjectsUp - 1));
-        engine.world.emplace<KinematicComponent>(object);
+        engine.world.emplace<KinematicComponent>(object, Vec2(speedObject, 0));
         engine.world.emplace<RigidBodyComponent>(object, 1.0f, obj_ref);
         engine.world.emplace<NameGroupComponent>(object, "Up " + std::to_string(i), "UPS");
 
@@ -39,7 +41,7 @@ int main(int argc, char *argv[]) {
 
         const auto object = engine.world.create();
         engine.world.emplace<TransformComponent>(object, Vec2(700, 725) - Vec2(600, 0) * (i - 1) / (numObjectsDown - 1));
-        engine.world.emplace<KinematicComponent>(object);
+        engine.world.emplace<KinematicComponent>(object, Vec2(-speedObject, 0));
         engine.world.emplace<RigidBodyComponent>(object, 1.0f, obj_ref);
         engine.world.emplace<NameGroupComponent>(object, "Down " + std::to_string(i), "DOWNS");
 
@@ -53,7 +55,7 @@ int main(int argc, char *argv[]) {
 
         const auto object = engine.world.create();
         engine.world.emplace<TransformComponent>(object, Vec2(725, 100) + Vec2(0, 600) * (i - 1) / (numObjectsRight - 1));
-        engine.world.emplace<KinematicComponent>(object);
+        engine.world.emplace<KinematicComponent>(object, Vec2(0, -speedObject));
         engine.world.emplace<RigidBodyComponent>(object, 1.0f, obj_ref);
         engine.world.emplace<NameGroupComponent>(object, "Right " + std::to_string(i), "RIGHTS");
 
@@ -67,7 +69,7 @@ int main(int argc, char *argv[]) {
 
         const auto object = engine.world.create();
         engine.world.emplace<TransformComponent>(object, Vec2(75, 700) - Vec2(0, 600) * (i - 1) / (numObjectsLeft - 1));
-        engine.world.emplace<KinematicComponent>(object);
+        engine.world.emplace<KinematicComponent>(object, Vec2(0, speedObject));
         engine.world.emplace<RigidBodyComponent>(object, 1.0f, obj_ref);
         engine.world.emplace<NameGroupComponent>(object, "Left " + std::to_string(i), "LEFTS");
 
@@ -89,62 +91,89 @@ int main(int argc, char *argv[]) {
     float speed = 150;
 
     std::string follow = "";
+    std::string followName = "";
 
     while (engine.NextFrame()) {
         engine.Update();
 
         // Setting up the timers
         float dt = engine.GetDeltaTime();
-        float seconds = SDL_GetTicks()/1000.0f;
-        float timer_speed = 3.0f;
-        float t = 1 - fabs(fmod(seconds/timer_speed, 2) - 1);
 
         // Logic
         if (engine.keyboard->upKeyPressed) {
             follow = "UPS";
+            std::cout << "Followed group: " << follow << std::endl;
         } else if (engine.keyboard->downKeyPressed) {
             follow = "DOWNS";
+            std::cout << "Followed group: " << follow << std::endl;
         } else if (engine.keyboard->rightKeyPressed) {
             follow = "RIGHTS";
+            std::cout << "Followed group: " << follow << std::endl;
         } else if (engine.keyboard->leftKeyPressed) {
             follow = "LEFTS";
+            std::cout << "Followed group: " << follow << std::endl;
         }
 
         Vec2 minDistance(1000, 1000);
 
-        auto view = engine.world.view<TransformComponent, NameGroupComponent>();
+        auto view = engine.world.view<TransformComponent, KinematicComponent, NameGroupComponent>();
         auto& transformObject = view.get<TransformComponent>(star);
 
         for (auto entity: view) {
             const auto nameGroup = view.get<NameGroupComponent>(entity);
             auto& transform = view.get<TransformComponent>(entity);
+            auto& kinem = view.get<KinematicComponent>(entity);
 
             if (nameGroup.group == "UPS") {
                 start_position  = Vec2(100, 75);
                 end_position  = Vec2(700, 75);
+
+                if (transform.position.x > end_position.x) {
+                    kinem.velocity = Vec2(-speedObject, 0);
+                } else if (transform.position.x < start_position.x) {
+                    kinem.velocity = Vec2(speedObject, 0);
+                }
             }
             else if (nameGroup.group == "DOWNS") {
                 start_position  = Vec2(700, 725);
                 end_position  = Vec2(100, 725);
+
+                if (transform.position.x < end_position.x) {
+                    kinem.velocity = Vec2(speedObject, 0);
+                } else if (transform.position.x > start_position.x) {
+                    kinem.velocity = Vec2(-speedObject, 0);
+                }
             }
             else if (nameGroup.group == "RIGHTS") {
                 start_position  = Vec2(725, 100);
                 end_position  = Vec2(725, 700);
+
+                if (transform.position.y > end_position.y) {
+                    kinem.velocity = Vec2(0, -speedObject);
+                } else if (transform.position.y < start_position.y) {
+                    kinem.velocity = Vec2(0, speedObject);
+                }
             }
             else {
                 start_position  = Vec2(75, 700);
                 end_position  = Vec2(75, 100);
-            }
 
-            new_position = Vec2::Lerp(start_position, end_position, MathUtils::easeInSine(t));
-            transform.position = new_position;
+                if (transform.position.y < end_position.y) {
+                    kinem.velocity = Vec2(0, speedObject);
+                } else if (transform.position.y > start_position.y) {
+                    kinem.velocity = Vec2(0, -speedObject);
+                }
+            }
 
             if (nameGroup.group == follow) {
                 if (minDistance.Magnitude() > (transformObject.position - transform.position).Magnitude()) {
                     minDistance = transformObject.position - transform.position;
+                    followName = nameGroup.name;
                 }
             }
         }
+
+        std::cout << "Followed name: " << followName << std::endl;
 
         if (minDistance != Vec2(1000, 1000)) {
             transformObject.position -= minDistance.UnitVector() * speed * dt;
