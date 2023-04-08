ac7afc8f99b60e0b2a4f0ae476d24af98bdc1e00
lauferhab <lferrerhaba@al.uloyola.es>
2023-04-08 19:55:13 +0200

    update exercise_12

--- MorgulEngine/exercises/exercise-12/main.cpp
+++ MorgulEngine/exercises/exercise-12/main.cpp
@@ -1,23 +1,6 @@
 #include "MorgulEngine.hh"
 #include <cstdlib>
 
-void OnKeyDown(const KeyDownEvent& keyDown) noexcept {
-    std::cout << "KeyDown event" << keyDown.symbol << std::endl;
-
-    if (keyDown.symbol == SDLK_UP) {
-        std::cout << "Pressed UP arrow" << std::endl;
-    }
-    if (keyDown.symbol == SDLK_DOWN) {
-        std::cout << "Pressed DOWN arrow" << std::endl;
-    }
-    if (keyDown.symbol == SDLK_RIGHT) {
-        std::cout << "Pressed RIGHT arrow" << std::endl;
-    }
-    if (keyDown.symbol == SDLK_LEFT) {
-        std::cout << "Pressed LEFT arrow" << std::endl;
-    }
-}
-
 int main(int argc, char *argv[]) {
     int width = 800;
     int heigth = 800;
@@ -32,8 +15,7 @@ int main(int argc, char *argv[]) {
     engine.world.emplace<TransformComponent>(player, Vec2(width/2, heigth/2));
     engine.world.emplace<GridMovementComponent>(player, 100);
 
-    // Event
-    engine.eventBus.sink<KeyDownEvent>().connect<&OnKeyDown>();
+    int n = rand() % 4 + 5;
 
     while (engine.NextFrame()) {
         engine.Update();
@@ -42,7 +24,6 @@ int main(int argc, char *argv[]) {
         Graphics::DrawGrid(100, true, true);
 
         if (engine.keyboard->upKeyPressed) {
-            int n = rand() % 6 + 6;
             const auto transform = engine.world.get<TransformComponent>(player);
 
             RegularPolygonShape fig = RegularPolygonShape(radius, n, Color::Orange(), false);
@@ -56,7 +37,7 @@ int main(int argc, char *argv[]) {
         if (engine.keyboard->downKeyPressed) {
             const auto transform = engine.world.get<TransformComponent>(player);
 
-            CircleShape fig = CircleShape(radius, Color::Magenta(), true);
+            CircleShape fig = CircleShape(radius, Color::Magenta(), false);
             CircleShape &fig_ref = fig;
 
             const auto newObject = engine.world.create();
@@ -65,7 +46,6 @@ int main(int argc, char *argv[]) {
             engine.world.emplace<RigidBodyComponent>(newObject, 1.0f, fig_ref);
         }
         if (engine.keyboard->rightKeyPressed) {
-            int n = rand() % 6 + 6;
             const auto transform = engine.world.get<TransformComponent>(player);
 
             StarShape fig = StarShape(radius, n, Color::Cyan(), false);
@@ -77,7 +57,6 @@ int main(int argc, char *argv[]) {
             engine.world.emplace<RigidBodyComponent>(newObject, 1.0f, fig_ref);
         }
         if (engine.keyboard->leftKeyPressed) {
-            int n = rand() % 6 + 6;
             const auto transform = engine.world.get<TransformComponent>(player);
 
             StarShape fig = StarShape(radius, n, Color::Green(), true);
